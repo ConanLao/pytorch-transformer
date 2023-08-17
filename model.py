61c0393d6cd93b423df55963d9be503b78793179
clao13 <clao13@bloomberg.net>
2023-08-17 18:50:19 -0400

    .

--- model.py
+++ model.py
@@ -200,9 +200,9 @@ class MultiheadAttention(nn.Module):
     def forward(self, q_src, k_src, v_src : torch.Tensor, mask : torch.Tensor):
         def attention(q, k, v):
             d_k = q.shape[-1]
-            logits = q @ k.transpose(-1, -2) / (d_k ** 0.5)
+            # logits = q @ k.transpose(-1, -2) / (d_k ** 0.5)
             # Sqrt and ** gives the same result
-            # logits = q @ k.transpose(-1, -2) / math.sqrt(d_k)
+            logits = (q @ k.transpose(-1, -2)) / math.sqrt(d_k)
             if mask is not None:
                 # -1e9 and -float('inf') gave the the same result
                 # logits.masked_fill(mask == 0, -float('inf'))
