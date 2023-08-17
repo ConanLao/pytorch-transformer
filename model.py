368bc024c9df9e332873f1d06ea9b1e6d2d19da7
clao13 <clao13@bloomberg.net>
2023-08-17 18:42:33 -0400

    .

--- model.py
+++ model.py
@@ -130,58 +130,58 @@ class Residual(nn.Module):
     def forward(self, xb : torch.Tensor, sub_layer : nn.Module):
         return xb + self.dropout(sub_layer(self.ln(xb)))
 
-# class MultiheadAttention(nn.Module):
+class MultiheadAttention(nn.Module):
 
-#     def __init__(self, d_model: int, h: int, dropout: float) -> None:
-#         super().__init__()
-#         self.d_model = d_model # Embedding vector size
-#         self.h = h # Number of heads
-#         # Make sure d_model is divisible by h
-#         assert d_model % h == 0, "d_model is not divisible by h"
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model # Embedding vector size
+        self.h = h # Number of heads
+        # Make sure d_model is divisible by h
+        assert d_model % h == 0, "d_model is not divisible by h"
 
-#         self.d_k = d_model // h # Dimension of vector seen by each head
-#         self.w_q = nn.Linear(d_model, d_model) # Wq
-#         self.w_k = nn.Linear(d_model, d_model) # Wk
-#         self.w_v = nn.Linear(d_model, d_model) # Wv
-#         self.w_o = nn.Linear(d_model, d_model) # Wo
-#         self.dropout = nn.Dropout(dropout)
+        self.d_k = d_model // h # Dimension of vector seen by each head
+        self.w_q = nn.Linear(d_model, d_model) # Wq
+        self.w_k = nn.Linear(d_model, d_model) # Wk
+        self.w_v = nn.Linear(d_model, d_model) # Wv
+        self.w_o = nn.Linear(d_model, d_model) # Wo
+        self.dropout = nn.Dropout(dropout)
 
-#     @staticmethod
-#     def attention(query, key, value, mask, dropout: nn.Dropout):
-#         d_k = query.shape[-1]
-#         # Just apply the formula from the paper
-#         # (batch, h, seq_len, d_k) --> (batch, h, seq_len, seq_len)
-#         attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
-#         if mask is not None:
-#             # Write a very low value (indicating -inf) to the positions where mask == 0
-#             attention_scores.masked_fill_(mask == 0, -1e9)
-#         attention_scores = attention_scores.softmax(dim=-1) # (batch, h, seq_len, seq_len) # Apply softmax
-#         if dropout is not None:
-#             attention_scores = dropout(attention_scores)
-#         # (batch, h, seq_len, seq_len) --> (batch, h, seq_len, d_k)
-#         # return attention scores which can be used for visualization
-#         return (attention_scores @ value), attention_scores
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+        # Just apply the formula from the paper
+        # (batch, h, seq_len, d_k) --> (batch, h, seq_len, seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            # Write a very low value (indicating -inf) to the positions where mask == 0
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim=-1) # (batch, h, seq_len, seq_len) # Apply softmax
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        # (batch, h, seq_len, seq_len) --> (batch, h, seq_len, d_k)
+        # return attention scores which can be used for visualization
+        return (attention_scores @ value), attention_scores
 
-#     def forward(self, q, k, v, mask):
-#         query = self.w_q(q) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
-#         key = self.w_k(k) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
-#         value = self.w_v(v) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        key = self.w_k(k) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        value = self.w_v(v) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
 
-#         # (batch, seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, h, seq_len, d_k)
-#         query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
-#         key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
-#         value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+        # (batch, seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
 
-#         # Calculate attention
-#         x, self.attention_scores = MultiheadAttention.attention(query, key, value, mask, self.dropout)
+        # Calculate attention
+        x, self.attention_scores = MultiheadAttention.attention(query, key, value, mask, self.dropout)
         
-#         # Combine all the heads together
-#         # (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
-#         x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+        # Combine all the heads together
+        # (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
 
-#         # Multiply by Wo
-#         # (batch, seq_len, d_model) --> (batch, seq_len, d_model)  
-#         return self.w_o(x)
+        # Multiply by Wo
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_model)  
+        return self.w_o(x)
 
 
 class MultiheadAttention(nn.Module):
@@ -200,14 +200,15 @@ class MultiheadAttention(nn.Module):
     def forward(self, q_src, k_src, v_src : torch.Tensor, mask : torch.Tensor):
         def attention(q, k, v):
             d_k = q.shape[-1]
-            logits = (q @ k.transpose(-1, -2)) / (d_k ** 0.5)
+            logits = q @ k.transpose(-1, -2) / (d_k ** 0.5)
             # Sqrt and ** gives the same result
             # logits = q @ k.transpose(-1, -2) / math.sqrt(d_k)
             if mask is not None:
                 # -1e9 and -float('inf') gave the the same result
                 # logits.masked_fill(mask == 0, -float('inf'))
                 logits.masked_fill(mask == 0, -1e9)
-            probs = F.softmax(logits, dim = -1)
+            # probs = F.softmax(logits, dim = -1)
+            probs = logits.softmax(dim=-1)
             if self.dropout is not None:
                 probs = self.dropout(probs)
             
